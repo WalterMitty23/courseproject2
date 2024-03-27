9250c981e0930f3c387983fb289af3b0964d0812
WalterMitty23 <23alexold23@gmail.com>
2024-03-27 03:13:59 -0400

    1.0

---
+++ src/main/java/com/example/courseproject2/Courseproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.courseproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courseproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Courseproject2Application.class, args);
+	}
+
+}
